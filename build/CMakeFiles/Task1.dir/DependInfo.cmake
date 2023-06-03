cc0fa6b3d86eed7a71d4e2044c222864bf281819
megaproes <sasha.borchenko@gmail.com>
2023-06-03 13:59:49 +0300

    Some changes on cmake

---
+++ build/CMakeFiles/Task1.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "F:/VS_Code_2/01_HomeWork/Task1/include/pugixml-master/pugixml.cpp" "CMakeFiles/Task1.dir/include/pugixml-master/pugixml.cpp.obj" "gcc" "CMakeFiles/Task1.dir/include/pugixml-master/pugixml.cpp.obj.d"
+  "F:/VS_Code_2/01_HomeWork/Task1/source/DirectoryReader/DirectoryReader.cpp" "CMakeFiles/Task1.dir/source/DirectoryReader/DirectoryReader.cpp.obj" "gcc" "CMakeFiles/Task1.dir/source/DirectoryReader/DirectoryReader.cpp.obj.d"
+  "F:/VS_Code_2/01_HomeWork/Task1/source/ReportFactory/ReportFactory.cpp" "CMakeFiles/Task1.dir/source/ReportFactory/ReportFactory.cpp.obj" "gcc" "CMakeFiles/Task1.dir/source/ReportFactory/ReportFactory.cpp.obj.d"
+  "F:/VS_Code_2/01_HomeWork/Task1/source/ReportGenerators/CsvReportGenerator.cpp" "CMakeFiles/Task1.dir/source/ReportGenerators/CsvReportGenerator.cpp.obj" "gcc" "CMakeFiles/Task1.dir/source/ReportGenerators/CsvReportGenerator.cpp.obj.d"
+  "F:/VS_Code_2/01_HomeWork/Task1/source/ReportGenerators/JsonReportGenerator.cpp" "CMakeFiles/Task1.dir/source/ReportGenerators/JsonReportGenerator.cpp.obj" "gcc" "CMakeFiles/Task1.dir/source/ReportGenerators/JsonReportGenerator.cpp.obj.d"
+  "F:/VS_Code_2/01_HomeWork/Task1/source/ReportGenerators/ReportGenerator.cpp" "CMakeFiles/Task1.dir/source/ReportGenerators/ReportGenerator.cpp.obj" "gcc" "CMakeFiles/Task1.dir/source/ReportGenerators/ReportGenerator.cpp.obj.d"
+  "F:/VS_Code_2/01_HomeWork/Task1/source/ReportGenerators/TxtReportGenerator.cpp" "CMakeFiles/Task1.dir/source/ReportGenerators/TxtReportGenerator.cpp.obj" "gcc" "CMakeFiles/Task1.dir/source/ReportGenerators/TxtReportGenerator.cpp.obj.d"
+  "F:/VS_Code_2/01_HomeWork/Task1/source/ReportGenerators/XmlReportGenerator.cpp" "CMakeFiles/Task1.dir/source/ReportGenerators/XmlReportGenerator.cpp.obj" "gcc" "CMakeFiles/Task1.dir/source/ReportGenerators/XmlReportGenerator.cpp.obj.d"
+  "F:/VS_Code_2/01_HomeWork/Task1/source/main.cpp" "CMakeFiles/Task1.dir/source/main.cpp.obj" "gcc" "CMakeFiles/Task1.dir/source/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
